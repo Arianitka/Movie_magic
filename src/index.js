b9fb5990f0d0e98068a3f5b2e919a91217543d0a
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-06-03 22:43:35 +0300

    extract express and hbs configs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
 
+
+
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
 const routes = require('./routes');
 
 const app = express();
 const port = 5000;
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'views'))
-
-app.use(express.static('src/static'));
+configHandlebars(app);
+configExpress(app);
 
 app.use(routes);
 
