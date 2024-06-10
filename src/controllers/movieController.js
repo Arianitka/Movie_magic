0c5e1fcdcbb2da5c67b4df326636565e659a8ece
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-06-10 23:29:18 +0300

    add rating to details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -12,6 +12,15 @@ router.post('/create', (req,res) => {
     movieService.create(newMovie)
 
     res.redirect('/')
+});
+
+router.get('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId);
+
+    movie.ratingStars = '&#x2605;'.repeat(movie.rating)
+;
+    res.render('details', {movie})
 })
-module.exports = router;
+module.exports = router
 
