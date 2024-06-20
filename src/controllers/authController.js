c8d6299cc8c774965ab1208f39fd6922be648d63
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-06-20 18:42:59 +0300

    add registered user  to mongo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 
+const userService = require('../services/authService')
+
 router.get('/register', (req, res) => {
     res.render('auth/register')
 
 });
+
+router.post('register', async (req, res) => {
+const userData = req.body;
+await authService.registe(userData);
+console.log(userData);
+
+
+res.redirect('/auth/login');
+})
 
 
 module.exports = router;
