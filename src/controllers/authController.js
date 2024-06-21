5380da0a1b2a646a9b767f24f9262609e6490d2e
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-06-21 17:04:05 +0300

    generate token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const authService = require('../services/authService')
-const userService = require('../services/authService')
+const userService = require('../services/authService');
+
 
 router.get('/register', (req, res) => {
     res.render('auth/register')
@@ -18,7 +19,13 @@ res.redirect('/auth/login');
 
 router.get('/login', (req, res) => {
     res.render('auth/login')
-})
+});
 
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    const token = await authService.login(email, password);
+});
+    console.log(token);
+    res.redirect('/');
 
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,31 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt')
 
-exports.register = (userData) => {
-    console.log(userData);
-    User.create(userData);
+const SECRET = 'fsdff1fsdfdsf'
+
+
+exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+
+    const user = await User.findOne({email});
+
+    if (!user) {
+        throw new Error('Wrong username or password')
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if(!isValid) {
+        throw new Error('Wrong username or password')
+    }
+
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    }
+      const token = await jwt.sign(payload,SECRET, {expiresIn : '2h'});
+      
+return token;
+
 }
